566d4ce760136d34ca754d1be2855180065a72b3
leej3 <johnleenimh@gmail.com>
2019-07-29 20:43:54 -0400

    make some improvements to the parsing spec

--- R/afni_cli_parser.R
+++ R/afni_cli_parser.R
@@ -27,37 +27,89 @@ run_tool <- function(toolname,package="afnistats",catch_error=TRUE,user_args=com
   do.call(toolname,parsed_ops)
 }
 
+#'Generate a command line parser from function documentation
+#'
+#'If a section "CLI info" is contained in the help text for a function, this
+#'tool parses the information in order to create a command line parser. The
+#'formatting for the section should be input to \code{\link[tibble]{tribble}}
+#'(all characters preceding the first '~' are discarded). The output is a
+#'command-line parser that can process user arguments obtained from
+#'\code{commandArgs(trailing=TRUE)}. Positional arguments in the function become
+#'required arguments on the command line. Keyword arguments become long options
+#'with the same default as the R function.
+#'
+#'The column headings represent values that are passed to the add_argument
+#'method of \code{\link[argparse]{argparse}} and it must contain at least:
+#'\enumerate{ \item param, the argument name. This becomes the argument/option
+#'name in the command line tool \item nargs, the number of items expected to
+#'follow the parameter. \item metavar, a representation of the arguments input.
+#'This helps cue the user to what information they should provide. It is not
+#'required when nargs is 0 \item type, the expected type of the input.}
+#'
+#'Other columns that might be useful \enumerate{\item group_inc, space separated
+#'group labels to which the parameter belongs. All parameters in a group must be
+#'provided together \item group_ex, same as group_inc but denotes that
+#'parameters are mutually exclusive in their usage \item prefix, an alternative
+#'string to denote options if you wish to make your interface deviate from the
+#''--' of the conventional
+#'\href{https://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html}{
+#'GNU/LINUX interface}. \item other arguments to
+#'argparse.ArgumentParser$add_argument.}
+#'
+#'
+#'
+#'@param topic The help topic that should be parsed
+#'@param package The package in which the help topic is contained
+#'
+#'@return argparse:ArgumentParser
+#'@export
 create_parser_from_function <- function(topic="MBA",package="afnistats"){
   parsed_help <- parse_help_topic(topic,package)
   parser <- create_parser_from_help(parsed_help)
   parser
 }
 
+
+
 create_parser_from_help <- function(parsed_help){
 
   parser <- argparse::ArgumentParser(description=fix_help_string(parsed_help$description),add_help=FALSE)
   parser$add_argument('-help',action='help') # add AFNI's help flag
   df_arg <- get_df_arg(parsed_help)
-  df_arg %>%
+  df_arg %<>%
     dplyr::group_by(1:dplyr::n()) %>%
-    tidyr::nest() %>%
+    tidyr::nest()
+  df_arg %>%
     dplyr::mutate(parsed = purrr::map(data, ~ add_arg(.x,parser)))
   parser
 
 }
 
 add_arg <- function(df,parser){
-  # option flags that take no arguments are treated differently. nargs can be a character, hence the check for NA
-  if (!suppressWarnings(is.na(df$nargs == 0)) && df$nargs == 0){
+  # nargs can be a character
+  if (!df$nargs %in% c("?","+","*")){
+    df$nargs <- as.integer(df$nargs)
+    # option flags that take no arguments are treated differently.
+    if  (df$nargs == 0){
     df <- df %>%
       dplyr::select(-c(nargs,type,metavar,default)) %>%
       dplyr::mutate(action = 'store_true')
+    }
+    # required arguments can't have a default
+    if (df$required == TRUE){
+      df <- dplyr::select(df,-c(default))
+    }
   }
+
+
   # "Not required" is used to specify that the value should not be used to create the parser and is dropped accordingly
-  arg_list <- df %>%  dplyr::select_if(~ !is.character(.x) || stringr::str_detect(.x,"Not required",negate=TRUE))  %>% purrr::transpose() %>% tibble::deframe()
+  arg_list <- df %>%  dplyr::select_if(~ !is.character(.x) || stringr::str_detect(.x,"^Not required$",negate=TRUE))
+  arg_list <- arg_list %>% purrr::transpose() %>% tibble::deframe()
+  # arg_list <- df %>% purrr::transpose() %>% purrr::map(~purrr::discard(.x,is.na))
   do.call(parser$add_argument,c(stringr::str_c('-',df$dest),arg_list))
+  # This can be useful for debugging...
   help_val <- capture.output(parser$print_help())
-  if (sum(stringr::str_detect(help_val,'raise ValueError|NameError'))) {
+  if (sum(stringr::str_detect(help_val,'.{1,15}Error'))) {
     stop(stringr::str_c("parser broken for: ",arg_list$dest))
   }
 
@@ -68,32 +120,89 @@ fix_help_string <- function(string){
   stringr::str_replace_all(string,'\\n',' ')
 }
 
+
+parse_cli_info_str <- function(parsed_help){
+  help_sections <-  parsed_help[names(parsed_help) == "section"]
+  if (!length(help_sections)){return(tibble::tibble(dest=character()))}
+  tribble_str <- help_sections %>%
+    purrr::keep(~ stringr::str_detect(.x,stringr::regex("cli ?info", ignore_case = TRUE))) %>%
+    stringr::str_split('~',n=2) %>%
+    purrr::flatten() %>%
+    .[[2]]
+
+  tribble_str %>%
+    stringr::str_replace_all("~",'') %>%
+    stringr::str_replace("\\n","empty\n") %>%
+    readr::read_csv() %>%
+    dplyr::select(-empty)
+}
+
+
+get_arg_info_from_function_signature <- function(funcname){
+  formals(funcname) %>%
+  purrr::map(~ rlang::maybe_missing(.x,"Not required"))  # the 'missing' symbol is nasty, get rid of it
+}
+
+get_df_arg_new <- function(parsed_help){
+  arguments <- purrr::flatten(parsed_help['arguments'])
+  help_vals <- purrr::map_chr(arguments,'description') %>%
+    purrr::set_names(purrr::map(arguments,'arg'))
+
+  # Parse the cli info section for argparse arguments
+  df <- parse_cli_info_str(parsed_help)
+
+  # parse the function signature for required arguments and defaults
+  default_args <- get_arg_info_from_function_signature(parsed_help)
+
+    # dplyr::mutate_all(as.character)
+
+  # Construct full output
+  df %>%
+    dplyr::mutate(
+      help= help_vals[dest],
+      nargs = fix_nargs(nargs),
+      default = default_args[dest], # Get the default values for the arguments
+
+      required = ifelse(is.na(default),TRUE,FALSE),
+    )
+}
+
 get_df_arg <- function(parsed_help){
   arguments <- purrr::flatten(parsed_help['arguments'])
-  df_arg <- purrr::map(arguments,'description') %>%
+  cli_info <- parse_cli_info_str(parsed_help )
+  arg_defaults <- formals(parsed_help$alias)
+
+  df_init <- purrr::map(arguments,'description') %>%
     purrr::set_names(purrr::map(arguments,'arg')) %>%
-    tibble::enframe() %>%
-    dplyr::rename(dest = name) %>%
-    tidyr::separate(value,sep='\\(cli_info: ',c('help','metadata')) %>%
-    tidyr::separate('metadata',sep=',',c('metavar','type','nargs')) %>%
+    tibble::enframe(name="dest",value="help") %>%
+    dplyr::left_join(cli_info,by="dest")
+
+  check_func_sig_and_doc_args_match(parsed_help,df_init,arg_defaults)
+  df_arg <- df_init %>%
     dplyr::mutate(
-      nargs = fix_nargs(nargs),
       help = fix_help_string(help),
-      default = formals(parsed_help$alias)[dest], # Get the default values for the arguments
+      default = unname(arg_defaults[dest]), # Get the default values for the arguments
       default = purrr::map(default,~ rlang::maybe_missing(.x,"Not required")), # the 'missing' symbol is nasty, get rid of it
-      required = ifelse(suppressWarnings(stringr::str_detect(default,"Not required")),TRUE,"Not required")
-      )
+      required = dplyr::if_else(suppressWarnings(stringr::str_detect(default,"Not required")),TRUE,FALSE)
+    )
 
   df_arg
 }
 
-fix_nargs <- function(nargs){
-  nargs %>%
-    stringr::str_replace('\\)','') %>%
-    purrr::map(purrr::quietly(as.integer)) %>%
-    purrr::map_int('result') %>%
-    purrr::map( ~if (is.na(.x)){'+'} else{.x})
-}
+check_func_sig_and_doc_args_match <- function(parsed_help,df_init,arg_defaults){
+  x <- names(arg_defaults)
+  y <- df_init$dest
+  arg_diff <- c(setdiff(x,y),setdiff(y,x))
+  undocumented_args_error <- paste(
+    c('For the function ',
+      parsed_help$alias,
+      ', the following arguments were in the function signature but were not documented as ',
+      'arguments in the help: ',
+      arg_diff,
+      '. All arguments must be documented if you wish to produce a command line interface.'),
+  collapse = '')
+  if (length(arg_diff)) stop(undocumented_args_error)
+  }
 
 #' parse_help_topic
 #'
@@ -109,7 +218,7 @@ parse_help_topic <- function(topic,package_name){
   help_val
 }
 
-#' Title
+#' An example function for testing parsing to generate a cli
 #'
 #' Description
 #'
@@ -119,10 +228,20 @@ parse_help_topic <- function(topic,package_name){
 #' @param a Description of a
 #' @param b
 #'   Description of b
+#' @param c Description of c
 #' @section Important:
 #' Don't run with scissors!
+#' @section Cli info:
+#' See \code{\link{create_parser_from_function}} for details on this section.
+#' \preformatted{
+#' ~dest,  ~metavar,   ~type,         ~nargs,
+#' "x",    "STRING",   "character",   "1",
+#' "a",    "N",        "integer",     "1",
+#' "b",    "N",        "integer",     "1",
+#' "c",    "N",        "integer",     "+",
+#' }
 #' @export
-example_func_for_parsing <- function(x,a,b){
+example_func_for_parsing <- function(x,a=NULL,b=NA,c="a value"){
   print("sample")
 }
 
@@ -174,4 +293,12 @@ get_dev_help <- function(topic,package_name){
     rd <- tools::parse_Rd(help_file$path)
     rd
     }
+}
+
+expect_equal_param_df <- function(df1,df2){
+  expect_equal(df1$default,df2$default)
+  expect_equal(
+    df1 %>% dplyr::select(-default),
+    df2 %>% dplyr::select(-default)
+  )
 }
--- tests/testthat/test-afni_cli_parser.R
+++ tests/testthat/test-afni_cli_parser.R
@@ -1,5 +1,7 @@
 # Given a help topic, a parser is generated...
 
+
+
 test_that("get help from a loaded function", {
   rd <- get_help_topic("example_func_for_parsing","afnistats")
   parsed <- Rd2list(rd)
@@ -26,5 +28,55 @@ test_that("parse help topic from an installed package", {
 })
 
 
+test_that("default args can be missing, NA,or NULL but missing is converted to Not required",{
+  sig_test <- function(pos_arg,null_default=NULL,na_default=NA,int_arg=3,logical_arg=TRUE,char_arg="a char"){}
+  expected <- list(pos_arg="Not required",null_default=NULL,na_default=NA,int_arg=3,logical_arg=TRUE,char_arg="a char")
+  expect_equal(get_arg_info_from_function_signature(sig_test), expected)
+
+})
 
 
+
+
+test_that("get_df_arg should produce a df with all cols being chr vectors except for default,required",{
+  non_character_cols <- c('required','default')
+  sig_test <- function(arg_1,arg_2=NULL){}
+  parsed_help = list(alias=sig_test,arguments=list(list(arg="arg_1",description="arg_1 help"),list(arg="arg_2",description="arg_2 help")))
+  expected <- tibble::tribble(
+    ~dest,    ~help,         ~default,        ~required,
+    "arg_1",  "arg_1 help",  "Not required",  TRUE,
+    "arg_2",  "arg_2 help",   NULL,           FALSE,
+  ) %>% dplyr::mutate_at(dplyr::vars(-non_character_cols),as.character)
+  df_out <- get_df_arg(parsed_help)
+  expect_identical(df_out, expected)
+
+
+  expected <- tibble::tribble(
+    ~dest,  ~metavar,   ~type,         ~nargs,  ~default,         ~required,   ~help,
+    "x",    "STRING",   "character",   "1",     "Not required",   TRUE,         "Description for x",
+    "a",    "N",        "integer",     "1",     NULL,             FALSE,        "Description of a",
+    "b",    "N",        "integer",     "1",     NA,               FALSE,        "Description of b",
+    "c",    "N",        "integer",     "+",     "a value",        FALSE,        "Description of c",
+  )
+  parsed_help <- parse_help_topic("example_func_for_parsing","afnistats")
+  df_out <- get_df_arg(parsed_help)
+
+  expect_equal_param_df(df_out, expected)
+
+}
+)
+
+test_that("get_df_arg raises error if arguments in function signature are not documented in the roxygen block",{
+  parsed_help <- parse_help_topic("example_func_for_parsing","afnistats")
+  parsed_help$arguments[2] <- NULL
+  missing_doc_error_txt <- 'For the function example_func_for_parsing, the
+following arguments were in the function signature but were not documented as
+arguments in the help: a. All arguments must be documented if you wish to
+produce a command line interface.' %>% stringr::str_replace_all("\n",' ')
+  expect_error(get_df_arg(parsed_help),missing_doc_error_txt)
+}
+)
+
+test_that("run_tool works for an example function",{
+  run_tool("example_func_for_parsing",package="afnistats")
+})
