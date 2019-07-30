deae09d6cdcd33dd08b0da88dec645fe0f79be23
leej3 <johnleenimh@gmail.com>
2019-07-30 14:47:36 -0400

    update mba cli info

--- R/MBA.R
+++ R/MBA.R
@@ -72,7 +72,7 @@
 #'     white  - anything else
 #'
 #'@param dataTable Path to the file on disk that contains the data structure. The data structure should be a table in long format (cf. wide
-#' format) in R with a header as the first line. (cli_info: PATH or DATA_AS_STRING,character,1)
+#' format) in R with a header as the first line.
 #'@param prefix Prefix is used to specify output file names. The main output is
 #' a text with prefix appended with .txt and stores inference information
 #' for effects of interest in a tabulated format depending on selected
@@ -80,17 +80,17 @@
 #' visualization plots such as matrix plot, and saved R data in binary
 #' mode. The .RData can be used for post hoc processing such as customized
 #' processing and plotting. Remove the .RData file to save disk space once
-#' you deem such a file is no longer useful. (cli_info: PREFIX,character,1)
+#' you deem such a file is no longer useful.
 #'@param chains Specify the number of Markov chains. Make sure there are enough
 #' processors available on the computer. Most of the time 4 cores are good
 #' enough. However, a larger number of chains (e.g., 8, 12) may help achieve
 #' higher accuracy for posterior distribution. Choose 1 for a single-processor
-#' computer, which is only practical only for simple models. (cli_info: N,integer,1)
+#' computer, which is only practical only for simple models.
 #'@param iterations Specify the number of iterations per Markov chain. Choose 1000 (default)
 #' for simple models (e.g., one or no explanatory variables). If convergence
 #' problem occurs as indicated by Rhat being great than 1.1, increase the number of
 #' iterations (e.g., 2000) for complex models, which will lengthen the runtime.
-#' Unfortunately there is no way to predict the optimum iterations ahead of time. (cli_info: N,integer,1)
+#' Unfortunately there is no way to predict the optimum iterations ahead of time.
 #'@param model This option specifies the effects associated with explanatory
 #' variables. By default (without user input) the model is specified as
 #' 1 (Intercept). Currently only between-subjects factors (e.g., sex,
@@ -102,25 +102,25 @@
 #' with the ones used in the header of data table. A+B represents the
 #' additive effects of A and B, A:B is the interaction between A
 #' and B, and A*B = A+B+A:B. Subject as a variable should not occur in
-#' the model specification here. (cli_info: FORMULA,character,1)
+#' the model specification here.
 #'@param MD  This option indicates that there are missing data in the input. With n
 #' regions, at least n(n-1)/2 values are assumed from each subject in the
 #' input with no missing data (default). When missing data are present,
-#' invoke this option so that the program will handle it properly. (cli_info: ' ',logical,0)
+#' invoke this option so that the program will handle it properly.
 #'@param r2z  This option performs Fisher transformation on the response variable
 #' (column Y) if it is correlation coefficient. Do not invoke the option
 #' if the transformation has already been applied or the variable is
-#' not correlation coefficient. (cli_info: ' ',logical,0)
+#' not correlation coefficient.
 #'@param cVars Identify categorical (qualitive) variables (or
 #' factors) with this option. The list with more than one variable
 #' has to be separated with comma (,) without any other characters such
 #' as spaces and should be surrounded within (single or double) quotes.
-#' For example, -cVars "sex,site" (cli_info: CSV_STRING,character,1)
+#' For example, -cVars "sex,site"
 #'@param qVars Identify quantitative variables (or covariates) with
 #' this option. The list with more than one variable has to be
 #' separated with comma (,) without any other characters such as
 #' spaces and should be surrounded within (single or double) quotes.
-#' For example, -qVars "Age,IQ" (cli_info: CSV_STRING,character,1)
+#' For example, -qVars "Age,IQ"
 #'@param stdz Identify quantitative variables (or covariates) to be
 #' standardized. To obtain meaningful and interpretable results and to
 #' achieve better convergence of Markov chains with reasonable iterations,
@@ -130,13 +130,13 @@
 #' variables varies substantially between groups, it may make sense to
 #' standardize the variable within each group before plugging the values
 #' into the data table. Currently MBA does not offer the option to perform
-#' within-group standardization. (cli_info: CSV_STRING,character,1)
+#' within-group standardization.
 #'@param EOI Identify effects of interest in the output by specifying the
 #' variable names separated with comma (,). For example, -EOI \"sex,age\".
 #' By default the Intercept is considered to be an effect of interest.
 #' Currently only variables, not their interactions, can be directly
 #' requested for output. However, most interaction effects can be obtained by
-#' either properly coding the variables (see example 3) or post processing. (cli_info: CSV_STRING,character,1)
+#' either properly coding the variables (see example 3) or post processing.
 #'@param qContr Identify comparisons of interest between quantitative
 #' variables in the output separated with comma (,). It only allows for
 #' pair-wise comparisons between two quantitative variables. For example,
@@ -145,23 +145,22 @@
 #' will be provided in the output. Make sure that such comparisons are
 #' meaningful (e.g., with the same scale and unit. This can be used to
 #' formulate comparisons among factor levels if the user quantitatively
-#' codes the factor levels. (cli_info: CSV_STRING,character,1)
+#' codes the factor levels.
 #'@param Y This is used to specify the column name that is designated as
 #' as the response/outcome variable. The default (when this option is not
-#' invoked) is 'Y'. (cli_info: STRING,character,1)
+#' invoked) is 'Y'.
 #'@param Subj This is used to specify the column name that is designated as
 #' as the measuring unit variable (usually subject). The default (when this
-#' option is not invoked) is 'Subj'. (cli_info: STRING,character,1)
+#' option is not invoked) is 'Subj'.
 #'@param ROI1 This var_name is used to specify the column name that is designated as
 #' as the region variable for the first set of each region pair. The default
-#' (when this option is not invoked) is 'ROI1'. (cli_info: STRING,character,1)
+#' (when this option is not invoked) is 'ROI1'.
 #'@param ROI2 This var_name is used to specify the column name that is designated as
 #' as the region variable for the second set of each region pair. The default
-#' (when this option is not invoked) is 'ROI2'. (cli_info: STRING,character,1)
-#'@param do_not_fit_model Do not fit the model (cli_info: ' ',logical,0)
-#'@param verb Specify verbosity (cli_info: path,character,1)
-#'@param dbgArgs No longer used (cli_info: path,character,1)
-#'@param
+#' (when this option is not invoked) is 'ROI2'.
+#'@param do_not_fit_model Do not fit the model
+#'@param verb Specify verbosity
+#'@param dbgArgs No longer used
 #' @note 1) There should have at least four columns in the table. These minimum
 #' four columns can be in any order but with fixed and reserved with labels:
 #' 'Subj', 'ROI1', 'ROI2', and 'Y'. The two columns 'ROI1' and 'ROI2' are
@@ -192,6 +191,30 @@
 #' @export
 #' @import brms
 #' @example vignettes/MBA-demo.Rmd
+#' @section CLI info:
+#'See \code{\link{create_parser_from_function}} for details on this section.
+#' \preformatted{
+#'  ~dest,               ~metavar,                   ~type,        ~nargs,
+#'  "dataTable",         "PATH or DATA_AS_STRING",   "character",  1,
+#'  "prefix",            "PREFIX",                   "character",  1,
+#'  "chains",            "N",                        "integer",    1,
+#'  "iterations",        "N",                        "integer",    1,
+#'  "model",             "FORMULA",                  "character",  1,
+#'  "MD",                " ",                        "logical",    0,
+#'  "r2z",               " ",                        "logical",    0,
+#'  "cVars",             "CSV_STRING",               "character",  1,
+#'  "qVars",             "CSV_STRING",               "character",  1,
+#'  "stdz",              "CSV_STRING",               "character",  1,
+#'  "EOI",               "CSV_STRING",               "character",  1,
+#'  "qContr",            "CSV_STRING",               "character",  1,
+#'  "Y",                 "STRING",                   "character",  1,
+#'  "Subj",              "STRING",                   "character",  1,
+#'  "ROI1",              "STRING",                   "character",  1,
+#'  "ROI2",              "STRING",                   "character",  1,
+#'  "do_not_fit_model",  " ",                        "logical",    0,
+#'  "verb",              "path",                     "character",  1,
+#'  "dbgArgs",           "path",                     "character",  1,
+#'}
 MBA <- function(dataTable,prefix="result",chains=4,iterations=1000,model=1,MD=FALSE,
                 r2z=FALSE, cVars=NULL, qVars='Intercept', stdz=NULL, EOI='Intercept', qContr=NULL,
                 Y='Y', Subj='Subj', ROI1='ROI1', ROI2='ROI2',do_not_fit_model=FALSE, verb=0,dbgArgs=FALSE){
