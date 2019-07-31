75eddbf708fb18046cc6d3a4f50d93665bd435ca
leej3 <johnleenimh@gmail.com>
2019-07-31 11:06:34 -0400

    make mba test more basoc

--- tests/testthat/test-MBA.R
+++ tests/testthat/test-MBA.R
@@ -1,11 +1,9 @@
-test_that("basic call to MBA with small data", {
-  outdir <- tempfile()
-  print(outdir)
-  dir.create(outdir)
-  setwd(outdir)
-  data_path <- system.file("extdata","tiny_data.txt",package = "afnistats")
-  MBA(dataTable = data_path,chains = 2,iterations = 100)
-  unlink(outdir,recursive=TRUE)
-})
+example_model_output <- file.path(getwd(),'model_example.RData')
 
+test_that("basic call to MBA without model fit works", {
+  data_path <- system.file("extdata","tiny_data.txt",package = "afnistats")
+  model <- MBA(prefix='result_from_mba',dataTable = data_path,do_not_fit_model=TRUE)
+  expect_equal(model,NA)
+
+})
 
