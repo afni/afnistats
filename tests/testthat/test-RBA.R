d9059e3e180036f51b3fddb07c8cc6b325560bd9
leej3 <johnleenimh@gmail.com>
2019-08-01 10:22:23 -0400

    fix formatting for test-RBA.R

--- tests/testthat/test-RBA.R
+++ tests/testthat/test-RBA.R
@@ -1,7 +1,5 @@
 test_that("basic call to MBA without model fit works", {
-  data_path <- system.file("extdata","tiny_data.txt",package = "afnistats")
-  model <- RBA(prefix='result_from_rba',dataTable = data_path,do_not_fit_model=TRUE)
-  expect_equal(model,NA)
-
+  data_path <- system.file("extdata", "tiny_data.txt", package = "afnistats")
+  model <- RBA(prefix = "result_from_rba", dataTable = data_path, do_not_fit_model = TRUE)
+  expect_equal(model, NA)
 })
-
