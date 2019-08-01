86622cfdb07c54ce9b2c5c6269646cd6eadb074a
leej3 <johnleenimh@gmail.com>
2019-08-01 10:22:55 -0400

    fix formatting for test-MBA.R

--- tests/testthat/test-MBA.R
+++ tests/testthat/test-MBA.R
@@ -1,9 +1,7 @@
-example_model_output <- file.path(getwd(),'model_example.RData')
+example_model_output <- file.path(getwd(), "model_example.RData")
 
 test_that("basic call to MBA without model fit works", {
-  data_path <- system.file("extdata","tiny_data.txt",package = "afnistats")
-  model <- MBA(prefix='result_from_mba',dataTable = data_path,do_not_fit_model=TRUE)
-  expect_equal(model,NA)
-
+  data_path <- system.file("extdata", "tiny_data.txt", package = "afnistats")
+  model <- MBA(prefix = "result_from_mba", dataTable = data_path, do_not_fit_model = TRUE)
+  expect_equal(model, NA)
 })
-
